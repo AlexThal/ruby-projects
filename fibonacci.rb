5d67439ee2721b4b30015ec785e1fa180940f6c0
Affonso Neto <affonso.neto@redbull.com>
2023-10-08 17:37:24 +0200

    Fibonacci exercise
    @alex do it

---
+++ fibonacci.rb
@@ -0,0 +1,9 @@
+# https://en.wikipedia.org/wiki/Fibonacci_sequence
+def fibonacci(n)
+  return 0
+end
+
+puts fibonacci(4) == 3
+puts fibonacci(19) == 4181
+puts fibonacci(0) == 0
+puts fibonacci(10) == 55
