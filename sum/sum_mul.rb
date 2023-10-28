e70b41b520d0b50cc3918b5c48ee814f6bd4457d
Affonso Neto <affonso.neto@redbull.com>
2023-10-28 15:37:56 +0200

    Add sum exercises

---
+++ sum/sum_mul.rb
@@ -0,0 +1,15 @@
+
+# Write a function receive a number n and x
+# and that outputs the sum of 1 to n
+# ONLY if n is a multiple of  x
+# ex: sum_mul(2, 1) = 1+2 = 3
+# ex: sum_mul(10, 5) = 5+10 = 15
+def sum_mul(n, x)
+  return 0
+end
+
+
+puts sum_mul(2, 1) == 3
+puts sum_mul(3, 3) == 3
+puts sum_mul(10, 5) == 15
+# Write more tests
