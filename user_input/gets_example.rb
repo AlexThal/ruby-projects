3474821c4a018e8e80aad25335f0753f9cd3032d
Affonso Neto <affonso.neto@redbull.com>
2023-10-28 15:46:12 +0200

    Add greeting exercises

---
+++ user_input/gets_example.rb
@@ -0,0 +1,5 @@
+# gets is the function used to get user input in ruby 
+# Run this program to check an example
+
+name = gets
+puts 'printing '+ name
---
+++ user_input/gretings.rb
@@ -0,0 +1,6 @@
+
+#Make a function that gets the user name and prints a greeting to him
+# ex. greet('Alex') -> Prints "Hello, Alex!"
+def greet(user)
+  nil
+end
